334a74d0b38fea4f18226b273a849836a06b14c9
RelampagoMinato <julioandreguedes@gmail.com>
2024-04-26 18:47:02 -0300

    Comentario Programa3

--- Programa3/src/br/edu/up/telas/Menu.java
+++ Programa3/src/br/edu/up/telas/Menu.java
@@ -1,40 +1,50 @@
 package br.edu.up.telas;
-import java.util.Scanner;
+
 import br.edu.up.controles.ControleEstande;
-import br.edu.up.modelos.Livro;;
+import br.edu.up.modelos.Livro;
+import java.util.Scanner;
+
 public class Menu {
+    // Método para executar o menu da estante de livros
     public void run (){
-        System.out.println("em uma estande de livros tem 5 modulares");
+        // Mensagem inicial
+        System.out.println("Em uma estante de livros tem 5 modulares");
         System.out.println("-------------------------------------------");
         System.out.println("| 1. Push = inserir livro                 |");
         System.out.println("| 2. Pop = remover livro                  |");
-        System.out.println("| 3.  Peek = visualizar livro             |");
+        System.out.println("| 3. Peek = visualizar livro               |");
         System.out.println("| 4. Listar = listar todos os livros      |");
         System.out.println("| 5. Sair                                 |");
         System.out.println("-------------------------------------------");
+
+        // Instanciação do controle da estante e do scanner
         ControleEstande control = new ControleEstande();
         Scanner scanner = new Scanner(System.in);
-        ControleEstande controle = new ControleEstande();
+
+        // Leitura da escolha do usuário
         int escolha = scanner.nextInt();
+
         switch (escolha) {
             case 1:
                 control.insert();
                 break;
             case 2:
-                
+                // Comentário 1: Adicione aqui a lógica para remover um livro da estante
                 break;
             case 3:
-                
+                // Comentário 2: Adicione aqui a lógica para visualizar um livro da estante sem removê-lo
                 break;
             case 4:
-            Livro[] books = control.getLivro();
+                // Listar todos os livros da estante
+                Livro[] books = control.getLivro();
                 for (int i = 0; i < 5; i++) {
                     Livro bi = books[i];
-                    String srt = " Titulo : " + bi.getTitulo() + " autores : " + bi.getAutores() + " Ano: " + bi.getAno() + " Codigo " + bi.getCodigo() + "isbn: " + bi.getIsbn();
+                    String srt = " Título: " + bi.getTitulo() + " Autores: " + bi.getAutores() + " Ano: " + bi.getAno() + " Código: " + bi.getCodigo() + " ISBN: " + bi.getIsbn();
                     System.out.println(srt);
                 }
                 break;
             case 5:
+                // Sair do menu
                 break;
             default:
                 break;
