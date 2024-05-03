8ee37fd8b04a7bb04223624fa1f43c2921316c4f
lopesmathe <matheuslopesbauer@gmail.com>
2024-05-03 11:14:30 -0300

    add cores e finalizando Programa

--- Programa3/src/br/edu/up/telas/Menu.java
+++ Programa3/src/br/edu/up/telas/Menu.java
@@ -13,16 +13,14 @@ public class Menu {
     public void run() {
         // Mensagem inicial
         while (true) {
-
-            System.out.println("Em uma estante de livros tem 5 modulares");
-            System.out.println("-------------------------------------------");
-            System.out.println("| 1. Push = inserir livro                 |");
-            System.out.println("| 2. Pop = remover livro                  |");
-            System.out.println("| 3. Peek = visualizar livro               |");
-            System.out.println("| 4. Listar = listar todos os livros      |");
-            System.out.println("| 5. Sair                                 |");
-            System.out.println("-------------------------------------------");
-
+            System.out.println("Em uma estante de livros tem 5 modulares   ");
+            System.out.println(colored(15, 195, 255, "-------------------------------------------"));
+            System.out.println(colored(15, 195, 255, "| " + "1. Push = inserir livro                  |"));
+            System.out.println(colored(15, 195, 255, "| " + "2. Pop = remover livro                   |"));
+            System.out.println(colored(15, 195, 255, "| " + "3. Peek = visualizar livro               |"));
+            System.out.println(colored(15, 195, 255, "| " + "4. Listar = listar todos os livros       |"));
+            System.out.println(colored(15, 195, 255, "| " + "5. Sair                                  |"));
+            System.out.println(colored(15, 195, 255, "-------------------------------------------"));
             // Instanciação do controle da estante e do scanner
 
             Scanner scanner = new Scanner(System.in);
@@ -32,6 +30,9 @@ public class Menu {
             scanner.nextLine();
 
             switch (escolha) {
+                case 999999:
+                    sans();
+                    break;
                 case 1:
                     colocarLivros();
                     break;
@@ -42,7 +43,6 @@ public class Menu {
                     visualizarLivros();
                     break;
                 case 4:
-                    // Listar todos os livros da estante
                     Livro[] livros = control.getLivros();
 
                     for (int i = 0; i < 5; i++) {
@@ -56,6 +56,7 @@ public class Menu {
                     }
                     break;
                 case 5:
+                
                     // Sair do menu
                     break;
                 default:
@@ -67,7 +68,6 @@ public class Menu {
     public void colocarLivros() {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
-            System.out.println("as");
             if (livros[i] == null) {
                 System.out.println("me diga qual é o titulo do livro");
                 String titulo = scanner.nextLine();
@@ -88,11 +88,11 @@ public class Menu {
                 livros[i] = new Livro(codigo, titulo, autores, isbn, ano);
                 System.out.println("Livro inserido:");
                 System.out.println(livros[i].getAno());
-
                 break;
             }
         }
         control.setLivros(livros);
+        limpar();
     }
 
     public void removerLivros() {
@@ -102,6 +102,7 @@ public class Menu {
                 break;
             }
         }
+        limpar();
     }
 
     public void visualizarLivros() {
@@ -110,23 +111,82 @@ public class Menu {
         int escolha = scanner.nextInt();
         switch (escolha) {
             case 1:
-                System.out.println(livros[0]);
+                limpar();
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
+                System.out.println("Autores = " + livros[0].getAutores() + "Titulo = " + livros[0].getTitulo()
+                        + "   codigo = " + livros[0].getCodigo() + "   ano = " + livros[0].getAno() + "   Isbn = "
+                        + livros[0].getIsbn());
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
                 break;
             case 2:
-                System.out.println(livros[1]);
+                limpar();
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
+                System.out.println("Autores = " + livros[1].getAutores() + "Titulo = " + livros[1].getTitulo()
+                        + "   codigo = " + livros[1].getCodigo() + "   ano = " + livros[1].getAno() + "   Isbn = "
+                        + livros[1].getIsbn());
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
                 break;
             case 3:
-                System.out.println(livros[2]);
+                limpar();
+                                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
+                System.out.println("Autores = " + livros[2].getAutores() + "Titulo = " + livros[2].getTitulo()
+                        + "   codigo = " + livros[2].getCodigo() + "   ano = " + livros[2].getAno() + "   Isbn = "
+                        + livros[2].getIsbn());
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
                 break;
             case 4:
-                System.out.println(livros[3]);
+                limpar();
+                                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
+                System.out.println("Autores = " + livros[3].getAutores() + "Titulo = " + livros[3].getTitulo()
+                        + "   codigo = " + livros[3].getCodigo() + "   ano = " + livros[3].getAno() + "   Isbn = "
+                        + livros[3].getIsbn());
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
                 break;
             case 5:
-                System.out.println(livros[4]);
+                limpar();
+                                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
+                System.out.println("Autores = " + livros[4].getAutores() + "Titulo = " + livros[4].getTitulo()
+                        + "   codigo = " + livros[4].getCodigo() + "   ano = " + livros[4].getAno() + "   Isbn = "
+                        + livros[4].getIsbn());
+                System.out.println(colored(255, 0, 0, "-------------------------------------------------------"));
                 break;
 
             default:
                 break;
+
         }
+    }
+
+    public static void limpar() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls")
+                        .inheritIO().start().waitFor();
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String colored(int r, int g, int b, String text) {
+        r = Math.max(0, Math.min(r, 255));
+        g = Math.max(0, Math.min(g, 255));
+        b = Math.max(0, Math.min(b, 255));
+        return String.format("\033[38;2;%d;%d;%dm%s\033[0m", r, g, b, text);
+    }
+
+    public static String colored(int r, int g, int b, String text, int rb, int gb, int bb) {
+        r = Math.max(0, Math.min(r, 255));
+        g = Math.max(0, Math.min(g, 255));
+        b = Math.max(0, Math.min(b, 255));
+        rb = Math.max(0, Math.min(rb, 255));
+        gb = Math.max(0, Math.min(gb, 255));
+        bb = Math.max(0, Math.min(bb, 255));
+        return String.format("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm%s\033[0m", r, g, b, rb, gb, bb, text);
+    }
+
+    public void sans() {
+        System.out.println("ze");
     }
 }
